352e2686e37f4fe9d00633274d64f4e1fbb6578b
savashakan22 <savashakan22@gmail.com>
2024-03-11 11:23:43 +0300

    Changed pandas archaic method
    Replaced the archaic Pandas.DataFrame.append() with Pandas.concat()
    
    Changed some variable names

--- Library_Class.py
+++ Library_Class.py
@@ -10,26 +10,18 @@ import pandas as pd
 from os.path import isfile
 import numpy as np
 
-librarylog = pd.DataFrame({
-        'Date':             [],
-        'Hour':             [],
-        'Name':             [],
-        'Lastname':         [],
-        'Booknumber':       [],
-        'Bookname':         [],
-        'Return':           []
-                                })
+library_log = pd.DataFrame(columns= ['Date', 'Hour', 'Name', 'Lastname', 'Booknumber', 'Bookname', 'Return'])
 
 if isfile('Library_log.csv'):
-    librarylog = pd.read_csv('Library_log.csv')
+    library_log = pd.read_csv('Library_log.csv')
 
 else:
-    librarylog.to_csv('Library_log.csv')
+    library_log.to_csv('Library_log.csv')
     
 
 class Member:
     """Used for adding students to the log and managing their books. 
-        Komutlar:   Member.__init__(name, lastname)     -> Lets you do change on a register with the matching name
+        Method:     Member.__init__(name, lastname)     -> Lets you do change on a register with the matching name
                     Member.addbook(bookno, bookname)    -> Adds book on a student register
                     Member.returnbook(bookno, bookname) -> Changes the book to Return = 1 on the given register
                     Member.showbooks()                  -> Shows the books on the register with the "Return = 0"
@@ -44,26 +36,25 @@ class Member:
     def addbook(self, bookno, bookname):
         """Adds book to the register's name, also adds the date and the hour"""
         
-        global librarylog
-        librarylog = librarylog.append(pd.DataFrame({
+        global library_log
+        library_append = pd.DataFrame({
                                     'Date':             [str(datetime.now().strftime('%d-%m-%Y'))],
                                     'Hour':             [str(datetime.now().strftime('%H:%M:%S'))],
                                     'Name':             [self.name],
                                     'Lastname':         [self.lastname],
                                     'Booknumber':       [bookno],
                                     'Bookname':         [bookname],
-                                    'Return':           [0]
-                                        }))
-        
-        librarylog.to_csv("Library_log.csv")
+                                    'Return':           [0]})
+        library_log = pd.concat([library_log, library_append])
+        library_log.to_csv("Library_log.csv")
 
 
     def showbook(self):
         """Returns the students books as a dataframe"""
         
-        isim_es = np.logical_and(librarylog["Name"] == self.name, librarylog['Lastname'] == self.lastname)
+        isim_es = np.logical_and(library_log["Name"] == self.name, library_log['Lastname'] == self.lastname)
 
-        kg1 = librarylog.loc[np.logical_and(isim_es, librarylog['Return'] == 0), ['Date', 'Hour', 'Booknumber', 'Bookname'] ]
+        kg1 = library_log.loc[np.logical_and(isim_es, library_log['Return'] == 0), ['Date', 'Hour', 'Booknumber', 'Bookname'] ]
         
         return kg1
 
@@ -71,12 +62,12 @@ class Member:
     def returnbook(self, bookno):
         """Used for returning a student's unreturned book"""
         
-        a = librarylog['Booknumber'] == bookno
-        b = np.logical_and(a, librarylog['Name'] == self.name)
-        c = np.logical_and(b, librarylog['Return'] == 0)
-        d = np.logical_and(c, librarylog['Lastname'] == self.lastname)
+        a = library_log['Booknumber'] == bookno
+        b = np.logical_and(a, library_log['Name'] == self.name)
+        c = np.logical_and(b, library_log['Return'] == 0)
+        d = np.logical_and(c, library_log['Lastname'] == self.lastname)
         
-        librarylog.loc[c, 'Return'] = 1
+        library_log.loc[c, 'Return'] = 1
         
-        librarylog.to_csv("Library_log.csv")
+        library_log.to_csv("Library_log.csv")
         
